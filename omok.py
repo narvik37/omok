73979115742c68f1c134b81e03b43ecc03bb9930
Gyuhyeon <narvik37@naver.com>
2021-02-01 22:20:03 +0900

    first commit

---
+++ omok.py
@@ -0,0 +1,16 @@
+import pygame, sys
+from pygame.locals import *
+# from rule import *
+
+#바둑판 모양
+board_color1 = (153, 102, 000)
+board_color2 = (153, 102, 51)
+board_color3 = (204, 153, 000)
+board_color4 = (204, 153, 51)
+bg_color = (128, 128, 128)
+black = (0, 0, 0)
+blue = (0, 50, 255)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 200, 0)
+
